ec1d61bc061292e38529a3e252b260a8c9f97819
rmgriffin <r.maurice.griffin@gmail.com>
2020-10-30 00:05:07 -0400

    Annotation and beginning analysis

--- MentalHealth.R
+++ MentalHealth.R
@@ -19,6 +19,7 @@ renv::snapshot()
 rm(p,PKG)
 
 ## Load data
+# Download from google drive to directory "Data"
 setwd("~/Github/GZ")
 dir.create(file.path('Data'), recursive = TRUE)
 folder_url<-"https://drive.google.com/open?id=1yMjM2V09xyrQ2Oekv93IX8YsrNQjBDWg"
@@ -32,33 +33,30 @@ system.time(map(files$id,dl))
 setwd("..")
 rm(files, folder, folder_url)
 
+# Load data to R
 NDVI<-raster("./Data/NDVI_mean_Guangzhou.tif")
+NDVI<-NDVI/10000 # Rescaling
 Pop<-raster("./Data/chn_ppp_2020_guangzhou.tif")
+# Pop<-raster("./Data/population.tif") <--switch to this when wetland shapefile is available
 
-## Visualize data
+# Visualize data (auxiliary layers from Natural Earth for mapping, SF format)
 cs<-ne_download(scale = 10, type = "countries", returnclass = "sf")
 r<-ne_download(scale = 10, type = "rivers_lake_centerlines", category = "physical",  returnclass = "sf")
 l<-ne_download(scale = 10, type = "land", category = "physical",  returnclass = "sf")
 
-## Sample wetland polygon 
-wl<-st_sf(st_as_sfc(st_bbox(Pop)))
+## Mapping
+prj<-CRS("+init=epsg:32649") # WGS84/UTM49N
 
-# Function to identify UTM zone of coordinates
-lonlat2UTM = function(lonlat) {
-  utm = (floor((lonlat[1] + 180) / 6) %% 60) + 1
-  if(lonlat[2] > 0) {
-    utm + 32600
-  } else{
-    utm + 32700
-  }
-}
-st_coordinates(wl)
+# Reprojecting
+Pop<-projectRaster(Pop, crs=prj) 
+NDVI<-projectRaster(NDVI, crs=prj)
 
-EPSG_2_UTM <- lonlat2UTM(c(112.9246,22.62458))
-wl<-st_transform(wl, EPSG_2_UTM)
-wl<-st_buffer(wl, dist = -55000)
-wl_1km<-st_buffer(wl, dist = 1000)
+# Sample wetland polygon (replace with wetland polygon)
+wl<-st_sf(st_as_sfc(st_bbox(NDVI))) 
+wl<-st_buffer(wl, dist = -55000) # Making it small
+wl_1km<-st_buffer(wl, dist = 1000) # Buffering the polygon by 1km, the extent of the serviceshed of NDVI from Haizhu wetland
 
+# Maps
 p1<-tm_shape(l, bbox = st_bbox(Pop), ext = .85) +
   tm_polygons(border.col = "black", col = "gray") +
   tm_shape(Pop) +
@@ -89,7 +87,11 @@ tmap_arrange(p1,p2, nrow = 1)
 
 ## Analysis
 # 1. Raster to point for each pop cell within 1 km of wetland
+rwl_1km<-crop(Pop,wl_1km) # crop pop raster to serviceshed
+rwl_p<-rasterToPoints(rwl_1km,spatial = TRUE) # raster to points
+rwl_p<-st_as_sf(rwl_p) # SP object to SF (faster/modern spatial format)
 # 2. Buffer each point by 1km
+rwl_b<-st_buffer(rwl_p,dist=1000)
 # 3. Extract values of NDVI for old NDVI raster/new NDVI raster
 # 3a. Create new raster that changes all cells within the wetland to appropriate NDVI
 # 3b. Extract
