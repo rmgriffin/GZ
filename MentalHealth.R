76f5b1cd78d7533e1b8697a8033cdf5385d48434
rmgriffin <r.maurice.griffin@gmail.com>
2021-12-14 15:49:57 -0500

    Writing annual NPV layer to disk

--- MentalHealth.R
+++ MentalHealth.R
@@ -192,7 +192,7 @@ cval<-rwl_b %>% dplyr::select(cValue,geometry)
 cvalc<-st_centroid(cval)
 popc<-st_as_sf(rasterToPoints(rwl_1km, spatial = TRUE))
 
-#st_write(cvalc, "annualval.gpkg")
+#st_write(cvalc, "annualval.gpkg") # Writing annual NPV layer to disk
 #st_write(cvalc, "annualval.shp")
 
 # Population
